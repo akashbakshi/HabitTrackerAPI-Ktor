8351252edfb5eda4178290f6707fe9ab494ebf88
akashbakshi <akashbakshi@bubble-tech.com>
2020-01-16 19:01:45 -0500

    - Added config stuff for heroku build - commented code - added status code when returning response

--- src/Application.kt
+++ src/Application.kt
@@ -10,18 +10,19 @@ import io.ktor.jackson.jackson
 import io.ktor.features.ContentNegotiation
 
 fun main(args: Array<String>){
-    embeddedServer(Netty,commandLineEnvironment(args)).start(wait=true) // get heroku system env variable for the PORT if it's not found default to application.conf port we specified
+
+    embeddedServer(Netty,commandLineEnvironment(args)).start(wait=true) // Start a Netty Server, and use our applicaiton.conf file to fill the rest of the server info like PORt and modules
 }
 
 fun Application.module(){
 
-    install(ContentNegotiation) {
-        jackson {
+    install(ContentNegotiation) { // use ContentNegotiation similar to import or require statement in nodejs
+        jackson {//use jackson JSON library so we can parse incoming requests as JSON
             Compiler.enable()
         }
     }
 
-    routing{
-        habits()
+    routing{//all routing for all handled inside here
+        habits() // use the habits() function inside Habits.kt and use the routes defined inside that
     }
 }
--- src/Habit.kt
+++ src/Habit.kt
@@ -2,6 +2,7 @@ package com.akashbakshi
 
 import io.ktor.routing.*
 import io.ktor.application.*
+import io.ktor.http.HttpStatusCode
 import io.ktor.request.*
 import io.ktor.response.*
 import java.util.UUID
@@ -10,15 +11,15 @@ data class Habit(val id:UUID = UUID.randomUUID(), val name:String, val author:St
 
 var habits = mutableListOf<Habit>()
 
-fun Routing.habits(){
-    route("/habit"){
+fun Routing.habits(){ // function use to handle our routing for all habit related endpoints
+    route("/habit"){ // use prefix
         get("/"){
-            call.respond(habits)
+            call.respond(HttpStatusCode.OK,habits) // response with all our habit elements
         }
         post("/"){
-            val tmpHabit = call.receive<Habit>()
-            habits.add(tmpHabit)
-            call.respond(tmpHabit)
+            val tmpHabit = call.receive<Habit>() // parse the incoming request to our data class
+            habits.add(tmpHabit) // add it to our array/ db later on
+            call.respond(HttpStatusCode.Created,tmpHabit) // send response back to user with Created and the element they just added
         }
     }
 }
